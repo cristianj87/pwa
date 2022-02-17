1781e80b5a07d1e8eed3dee802b8a38b0ae39306
cristianj87 <cristian_utcj@hotmail.com>
2022-02-16 20:15:50 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registrado correctamente: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker error al registrarlo: ', err);
+    });
+  });
+}
